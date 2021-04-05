11059b857a27874175af64576939ee4d8253cf03
krystalmm <taeyeon_aina@live.jp>
2021-04-06 01:54:47 +0900

    [add] instructor1 seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Instructor.create!(
+  name: "Jean",
+  email: "jean@lybody.com",
+  password: "jeanpassword",
+  instructor_image: File.open("./app/assets/images/instructor1.png"),
+  mind: "楽しいworkoutを提供することを心がけています",
+  comment: "You can do it! 一緒に頑張りましょう!"
+)
