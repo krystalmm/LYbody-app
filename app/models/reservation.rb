4d6ff0a9e71d1948c268e9fccff3c9140f4ef802
krystalmm <taeyeon_aina@live.jp>
2021-04-09 23:12:46 +0900

    [add] reservation model validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,7 +2,24 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :instructor
 
+  validates :start_time, presence: true, uniqueness: true
+  validates :end_time, presence: true
+  validate :check_start_and_end_time
+  validate :check_start_time
+
   LESSON_HOUR = 1
+
+  def check_start_and_end_time
+    if self.start_time > self.end_time
+      errors.add(:end_time)
+    end
+  end
+
+  def check_start_time
+    if self.start_time < Time.now
+      errors.add(:start_time, "は現在の日時より遅い時間を選択してください。")
+    end
+  end
 
   def set_end_time
     self.end_time = self.start_time + LESSON_HOUR.hour
@@ -10,8 +27,8 @@ class Reservation < ApplicationRecord
 
   def date_and_time
     date = "#{start_time.year}年 #{start_time.month}月 #{start_time.day}日"
-    start_time = "#{self.start_time.hour} : #{self.start_time.min}"
-    end_time = "#{self.end_time.hour} : #{self.end_time.min}"
+    start_time = "#{self.start_time.hour} : #{self.start_time.strftime('%M')}"
+    end_time = "#{self.end_time.hour} : #{self.end_time.strftime('%M')}"
     "#{date}  #{start_time} ~ #{end_time}"
   end
 end
