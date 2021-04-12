3ed3c592206b18ccd2f879d704928cdaeaf1aded
krystalmm <taeyeon_aina@live.jp>
2021-04-12 11:00:20 +0900

    [update] chat page layout and [update] public chats controller

--- app/controllers/public/chats_controller.rb
+++ app/controllers/public/chats_controller.rb
@@ -1,2 +1,19 @@
 class Public::ChatsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @room = Room.find(params[:chat][:room_id])
+    @chat = Chat.new(chat_params)
+    if @chat.save
+      redirect_to room_path(@room)
+    else
+      redirect_to room_path(@room)
+    end
+  end
+
+  private
+
+  def chat_params
+    params.require(:chat).permit(:room_id, :is_user, :message)
+  end
 end
