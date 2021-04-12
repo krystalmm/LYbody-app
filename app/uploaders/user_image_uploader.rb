b1d947fa33e1e060e3477d93f1c25b60ce86a2b4
krystalmm <taeyeon_aina@live.jp>
2021-04-12 16:31:27 +0900

    [update] instructors chat icon

--- app/uploaders/user_image_uploader.rb
+++ app/uploaders/user_image_uploader.rb
@@ -40,7 +40,7 @@ class UserImageUploader < CarrierWave::Uploader::Base
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_allowlist
-    %w[jpg png]
+    %w[jpg jpeg png]
   end
 
   # Override the filename of the uploaded files:
