62c4c26a2c906dc6b0eb19df47439acb50e9b895
krystalmm <taeyeon_aina@live.jp>
2021-04-29 16:43:49 +0900

    [add] seed to reservation and user

--- db/fixtures/04_user.rb
+++ db/fixtures/04_user.rb
@@ -7,6 +7,7 @@ User.seed do |s|
   s.email = "taro@example.com"
   s.password = "taropassword"
   s.phone_number = "08011112222"
+  s.is_payed = true
 end
 
 User.seed do |s|
@@ -18,6 +19,7 @@ User.seed do |s|
   s.email = "michiko@example.com"
   s.password = "michikopassword"
   s.phone_number = "0611112222"
+  s.is_payed = false
 end
 
 User.seed do |s|
@@ -29,6 +31,7 @@ User.seed do |s|
   s.email = "yuto@example.com"
   s.password = "yutopassword"
   s.phone_number = "08000001111"
+  s.is_payed = true
 end
 
 User.seed do |s|
@@ -40,5 +43,31 @@ User.seed do |s|
   s.email = "yuka@example.com"
   s.password = "yukapassword"
   s.phone_number = "0622221111"
+  s.is_payed = true
 end
+
+User.seed do |s|
+  s.id = 5
+  s.firstname = "吉沢"
+  s.lastname = "柚乃"
+  s.kana_firstname = "ヨシザワ"
+  s.kana_lastname = "ユノ"
+  s.email = "yuno@example.com"
+  s.password = "yunopassword"
+  s.phone_number = "08055554444"
+  s.is_payed = true
+end
+
+User.seed do |s|
+  s.id = 6
+  s.firstname = "新田"
+  s.lastname = "晴"
+  s.kana_firstname = "ニッタ"
+  s.kana_lastname = "ハル"
+  s.email = "haru@example.com"
+  s.password = "harupassword"
+  s.phone_number = "0655553333"
+  s.is_payed = true
+end
+
 
