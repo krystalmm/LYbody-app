a452cbaf425052ee7e8c9bf4f9a20a58dba5f84e
krystalmm <taeyeon_aina@live.jp>
2021-05-01 23:06:12 +0900

    [add] public chats system spec

---
+++ spec/system/public/chats_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'Chats', type: :system, js: true do
+  let(:user) { FactoryBot.create(:user) }
+  let(:instructor) { FactoryBot.create(:instructor) }
+  let(:room) { FactoryBot.create(:room, user_id: user.id, instructor_id: instructor.id) }
+
+  before do
+    login_as(user)
+    visit room_path(room)
+  end
+
+  it 'succeeds create chat' do
+    fill_in 'chat-textarea', with: 'create chat'
+    click_button '送信'
+    aggregate_failures do
+      expect(current_path).to eq room_path(room)
+      expect(page).to have_css('p', text: 'create chat')
+    end
+  end
+
+  it 'fails create chat' do
+    fill_in 'chat-textarea', with: ' '
+    click_button '送信'
+    aggregate_failures do
+      expect(current_path).to eq room_path(room)
+      expect(page).to_not have_css('p', text: ' ')
+    end
+  end
+end
