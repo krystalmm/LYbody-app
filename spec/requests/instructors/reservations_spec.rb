d324695fd1e5721fcaef1c5bfef4a0eba3e0b0bd
krystalmm <taeyeon_aina@live.jp>
2021-05-09 15:52:47 +0900

    [add] instructors reservation request spec

--- spec/requests/instructors/reservations_spec.rb
+++ spec/requests/instructors/reservations_spec.rb
@@ -1,7 +1,43 @@
 require 'rails_helper'
 
-RSpec.describe 'Instructors::Reservations', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Reservations', type: :request do
+  let(:instructor) { FactoryBot.create(:instructor) }
+  let!(:reservation) { FactoryBot.create(:reservation, instructor_id: instructor.id) }
+
+  describe '#update' do
+    before { sign_in instructor }
+
+    it 'succeeds update reservation with valid datetime' do
+      patch instructors_reservation_path(reservation), params: { reservation: FactoryBot.attributes_for(:reservation) }
+      aggregate_failures do
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to instructors_mypage_path
+      end
+    end
+  end
+
+  describe '#destroy' do
+    before { sign_in instructor }
+
+    it 'succeeds destroy reservation' do
+      aggregate_failures do
+        expect do
+          delete instructors_reservation_path(reservation)
+        end.to change(Reservation, :count).by(-1)
+        expect(response).to redirect_to instructors_mypage_path
+      end
+    end
+  end
+
+  describe 'before_action: :authenticate_instructor' do
+    it 'redirects update when not logged in' do
+      patch instructors_reservation_path(reservation), params: { reservation: FactoryBot.attributes_for(:reservation) }
+      expect(response).to redirect_to new_instructor_session_path
+    end
+
+    it 'redirects destroy when not logged in' do
+      delete instructors_reservation_path(reservation)
+      expect(response).to redirect_to new_instructor_session_path
+    end
   end
 end
