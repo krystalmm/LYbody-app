3ad9946e8b3af7ecc77735244fc663d9e57142d2
krystalmm <taeyeon_aina@live.jp>
2021-05-10 15:39:14 +0900

    [add] public chats request spec

--- spec/requests/public/chats_spec.rb
+++ spec/requests/public/chats_spec.rb
@@ -1,7 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe 'Public::Chats', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Chats', type: :request do
+  let(:user) { FactoryBot.create(:user) }
+  let(:room) { FactoryBot.create(:room, user_id: user.id) }
+
+  describe '#create' do
+    before { sign_in user }
+
+    it 'succeeds create chat message' do
+      post chats_path, params: { chat: {
+        is_user: true, room_id: room.id, message: 'new message'
+      }, format: :js }
+      expect(response.body).to include 'new message'
+    end
+  end
+
+  describe 'before_action: :authenticate_user' do
+    it 'redirects create when not logged in' do
+      post chats_path, params: { chat: {
+        is_user: true, room_id: room.id, message: 'new message'
+      } }
+      expect(response).to redirect_to new_user_session_path
+    end
   end
 end
