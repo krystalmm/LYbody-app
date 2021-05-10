a4c1aa87ea61ef66285680bc15cd687d72669a26
krystalmm <taeyeon_aina@live.jp>
2021-05-10 16:24:10 +0900

    [add] instructors rooms request spec

--- spec/requests/instructors/rooms_spec.rb
+++ spec/requests/instructors/rooms_spec.rb
@@ -1,7 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe 'Instructors::Rooms', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Rooms', type: :request do
+  let(:instructor) { FactoryBot.create(:instructor) }
+  let(:room) { FactoryBot.create(:room, instructor_id: instructor.id) }
+
+  describe '#show' do
+    before { sign_in instructor }
+
+    it 'responds successfully' do
+      get instructors_room_path(room)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe '#pagination' do
+    before { sign_in instructor }
+
+    it 'responds successfully' do
+      get instructors_room_pagination_path(room, 2), xhr: true
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'before_action: :authenticate_instructor' do
+    it 'redirects show when not logged in' do
+      get instructors_room_path(room)
+      expect(response).to redirect_to new_instructor_session_path
+    end
+
+    it 'redirects pagination when not logged in' do
+      get instructors_room_pagination_path(room, 2)
+      expect(response).to redirect_to new_instructor_session_path
+    end
   end
 end
