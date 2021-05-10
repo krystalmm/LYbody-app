0ee24596893a51da3d1756a37fdeb77ca01e62c8
krystalmm <taeyeon_aina@live.jp>
2021-05-10 17:11:50 +0900

    [add] public rooms request spec

--- spec/requests/public/rooms_spec.rb
+++ spec/requests/public/rooms_spec.rb
@@ -1,7 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe 'Public::Rooms', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Rooms', type: :request do
+  let(:user) { FactoryBot.create(:user) }
+  let(:room) { FactoryBot.create(:room, user_id: user.id) }
+
+  describe '#show' do
+    before { sign_in user }
+
+    it 'responds successfully' do
+      get room_path(room)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe '#pagination' do
+    before { sign_in user }
+
+    it 'responds successfully' do
+      get room_pagination_path(room, 2), xhr: true
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'before_action: :authenticate_user' do
+    it 'redirects show when not logged in' do
+      get room_path(room)
+      expect(response).to redirect_to new_user_session_path
+    end
+
+    it 'redirects pagination when not logged in' do
+      get room_pagination_path(room, 2)
+      expect(response).to redirect_to new_user_session_path
+    end
   end
 end
