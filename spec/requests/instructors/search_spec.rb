4a25f811c9ad4786cc8d2fd79a306f1f7e77dccb
krystalmm <taeyeon_aina@live.jp>
2021-05-10 16:05:34 +0900

    [add] instructors search request spec

--- spec/requests/instructors/search_spec.rb
+++ spec/requests/instructors/search_spec.rb
@@ -1,7 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe "Instructors::Searches", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe "Search", type: :request do
+  let(:instructor) { FactoryBot.create(:instructor) }
+
+  describe "#search" do
+    before { sign_in instructor }
+
+    it 'responds successfully' do
+      get instructors_search_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'before_action: :authenticate_instructor' do
+    it 'redirects search when not logged in' do
+      get instructors_search_path
+      expect(response).to redirect_to new_instructor_session_path
+    end
   end
 end
