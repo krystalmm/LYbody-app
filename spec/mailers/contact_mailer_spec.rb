777b4873ee4e94e89433d86484e2101327a864ea
krystalmm <taeyeon_aina@live.jp>
2021-05-10 22:54:23 +0900

    [add] contact mailer spec

--- spec/mailers/contact_mailer_spec.rb
+++ spec/mailers/contact_mailer_spec.rb
@@ -1,5 +1,21 @@
 require "rails_helper"
 
 RSpec.describe ContactMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:contact) { FactoryBot.create(:contact) }
+  let(:mail) { ContactMailer.send_mail(contact) }
+
+  it 'renders the headers' do
+    expect(mail.subject).to eq('【お問い合わせ】')
+    expect(mail.to).to eq([ENV['LYBODYMAIL']])
+    expect(mail.from).to eq(['noreply-lybody@example.com'])
+  end
+
+  it 'renders the body' do
+    expect(mail.text_part.body.encoded).to match "#{contact.name} 様 から問い合わせがありました。"
+    expect(mail.html_part.body.encoded).to match "#{contact.name} 様 から問い合わせがありました。"
+    expect(mail.text_part.body.encoded).to match "【メールアドレス】: #{contact.email}"
+    expect(mail.html_part.body.encoded).to match "【メールアドレス】: #{contact.email}"
+    expect(mail.text_part.body.encoded).to match contact.message
+    expect(mail.html_part.body.encoded).to match contact.message
+  end
 end
